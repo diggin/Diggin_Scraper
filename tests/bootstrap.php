28517efa77df419744db29eee990b9f5b0724635
sasezaki <sasezaki@gmail.com>
2012-10-02 00:00:15 +0900

    use composer autoload instead of SplClassLoader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,15 +2,4 @@
 
 error_reporting( E_ALL | E_STRICT );
 
-$vendor = dirname(__DIR__).'/vendor';
-require_once $vendor.'/SplClassLoader.php';
-
-$loader = new SplClassLoader('Diggin\\Http\Charset', $vendor.'/Diggin_Http_Charset/src');
-$loader->register();
-$loader = new SplClassLoader('Diggin\\Scraper\\Adapter\\Htmlscraping', $vendor.'/Diggin_Scraper_Adapter_Htmlscraping/src');
-$loader->register();
-$loader = new SplClassLoader('Diggin\\Scraper', dirname(__DIR__).'/src');
-$loader->register();
-$loader = new SplClassLoader('Zend', dirname(__DIR__).'/vendor');
-$loader->register();
-
+require_once dirname(__DIR__).'/vendor/autoload.php';
